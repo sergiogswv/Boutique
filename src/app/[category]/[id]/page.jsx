a1cdb41a01135662db689a106f9f2111b46bc8e4
Sergio Guadarrama <sergio.gswv@outlook.com>
2023-12-30 16:29:55 -0600

    Change filter photos on one product

--- src/app/[category]/[id]/page.jsx
+++ src/app/[category]/[id]/page.jsx
@@ -1,13 +1,11 @@
 import ModuleProduct from '@/components/module/ModuleProduct'
-import { fetchFn } from '@/components/utils/fetchFn'
 import React from 'react'
 
-const ProductPage = async ({ params }) => {
-  const product = await fetchFn({ endpoint: `/products/product?id=${params.id}`, method: 'GET', body: null })
+const ProductPage = ({ params }) => {
   return (
     <div className='w-full h-auto md:h-[1000px] md:w-10/12 grid grid-rows-2 md:grid-cols-[1fr_500px] mx-auto px-1 py-3 md:p-5 gap-5'>
       <ModuleProduct
-        product={product}
+        id={params.id}
       />
 
     </div>
