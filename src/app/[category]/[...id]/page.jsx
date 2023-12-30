f91c7759c327b66e561365657503c695cfc0723c
Sergio Guadarrama <sergio.gswv@outlook.com>
2023-12-30 17:05:44 -0600

    Change filter photos on one product

--- src/app/[category]/[...id]/page.jsx
+++ src/app/[category]/[...id]/page.jsx
@@ -2,6 +2,7 @@ import ModuleProduct from '@/components/module/ModuleProduct'
 import React from 'react'
 
 const ProductPage = ({ params }) => {
+  console.log(params.id)
   return (
     <div className='w-full h-auto md:h-[1000px] md:w-10/12 grid grid-rows-2 md:grid-cols-[1fr_500px] mx-auto px-1 py-3 md:p-5 gap-5'>
       <ModuleProduct
