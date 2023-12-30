0723f2b5d07c717b4d3bf28f0467edf057d703f5
Sergio Guadarrama <sergio.gswv@outlook.com>
2023-12-30 12:45:43 -0600

    Change height

--- src/app/[category]/[id]/page.jsx
+++ src/app/[category]/[id]/page.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const ProductPage = async ({ params }) => {
   const product = await fetchFn({ endpoint: `/products/product?id=${params.id}`, method: 'GET', body: null })
   return (
-    <div className='w-full md:w-10/12 grid grid-rows-2 md:grid-cols-[1fr_500px] mx-auto px-1 py-3 md:p-5 gap-5'>
+    <div className='w-full h-auto md:h-[1000px] md:w-10/12 grid grid-rows-2 md:grid-cols-[1fr_500px] mx-auto px-1 py-3 md:p-5 gap-5'>
       <ModuleProduct
         product={product}
       />
